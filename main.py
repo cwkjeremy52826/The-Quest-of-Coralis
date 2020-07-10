4c61ca7d93900e3cc9b71d979bb99335d573588f
cwkjeremy52827 <reynolds_berets@comcast.net>
2020-07-10 18:24:03 +0000

    I added code

--- main.py
+++ main.py
@@ -40,7 +40,7 @@ if ans.upper() == "Y":
     if d2 == "A":
       print("You have chosen to " + Horse[d2] + "! Now lets hit the road.\n")
   print("You have left the city and all around you are green rolling hills. The journey goes quickly for the first day before you set up camp. You wake up the next morning and immediately hit the road again, but as you travel a thundering noise reaches your ears. As the sound gets louder you see 4 men clad in black riding towards you on scrawny looking horses, weapons raised.")
-  if d2 == "b":
+  if d1 == "A":
     print("You have no choice but to stay where you are and wait for them to get to you. As the riders near you recognize them for bandits.")
   if d2 == "A":
     d3 = input('Do you try to out run them on your horse or do you stay and see who those riders are? Type A if you want to run or type B if you want to stay.\n')
